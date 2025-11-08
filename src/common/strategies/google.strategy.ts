a4c0aa869cdf9374fe1a175131e5d841ad307d3f
heinthantzaw <heinthantzaw@ayainnovation.com>
2025-11-08 14:42:00 +0630

    [ENHANCEMENT]Change hard code to env callback url in google strategy

--- src/common/strategies/google.strategy.ts
+++ src/common/strategies/google.strategy.ts
@@ -11,8 +11,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      // callbackURL: process.env.GOOGLE_CALLBACK_URL!,
-      callbackURL: 'http://localhost:5173/auth/google/callback',
+      callbackURL: process.env.GOOGLE_CALLBACK_URL!,
       scope: ['email', 'profile'],
       passReqToCallback: true, // important
     } as any);
